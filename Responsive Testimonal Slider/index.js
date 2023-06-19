5a17feb11f0ff1f6b89339eeb8e004416951805e
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-06-20 01:05:57 +0530

    first commit

---
+++ Responsive Testimonal Slider/index.js
@@ -0,0 +1,15 @@
+// The following code is available in swiperJS website.
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
